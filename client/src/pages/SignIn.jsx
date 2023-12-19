94a13aefcecc2fac3ce6b3e63dc37bde2da84d68
Kal Tollosa <kalkidanh89@gmail.com>
2023-12-19 23:59:39 +0300

    installing redux

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-//import OAuth from '../components/OAuth';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
