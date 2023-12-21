0f03c605ad47c9453ff538c3a4961d44f62a5d9f
Kal Tollosa <kalkidanh89@gmail.com>
2023-12-21 22:15:43 +0300

    image pload fnction added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-green-500'>Housey</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
@@ -36,34 +36,26 @@ export default function Header() {
           className='bg-slate-100 p-3 rounded-lg flex items-center'
         >
           <input
-            type='text'
-            placeholder='Search...'
+            type='text' placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+            value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
           <button>
-            <FaSearch className='text-slate-600' />
+            <FaSearch className='text-green-600' />
           </button>
         </form>
         <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+          <Link to='/'> <li className='hidden sm:inline text-slate-700 hover:underline'>
               Home
             </li>
           </Link>
-          <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+          <Link to='/about'> <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
           </Link>
           <Link to='/profile'>
             {currentUser ? (
-              <img
-                className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
-                alt='profile'
-              />
+              <img className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar} alt='profile'/>
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
