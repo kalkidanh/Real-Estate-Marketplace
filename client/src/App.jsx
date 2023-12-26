ca48c9e0efbb7beb727ce66e4004f3860d747a9d
Kal Tollosa <kalkidanh89@gmail.com>
2023-12-26 22:26:01 +0300

    listing page UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return <BrowserRouter>
@@ -15,9 +16,9 @@ export default function App() {
     <Route path='/sign-in' element={<SignIn />} />
     <Route path='/sign-up' element={<SignUp />} />
     <Route path='/about' element={<About />} />
-    <Route path='/profile' element={<Profile />} />
     <Route element={<PrivateRoute />}>
-    <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
     </Route>
 
   </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'; 
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -137,9 +138,11 @@ export default function Profile() {
         <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'onChange={handleChange}/>
         <input type='password' placeholder='password' onChange={handleChange} id='password' className='border p-3 rounded-lg'/>
         <button disabled={loading}
-        className='bg-green-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'> 
+        className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'> 
         {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' 
+        to={"/create-listing"}> Create Listing</Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
