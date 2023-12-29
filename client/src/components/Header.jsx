9fc6dfc5d172fff453a0908bfdec1c9cfb976a9c
Kal Tollosa <kalkidanh89@gmail.com>
2023-12-29 22:17:44 +0300

    search page UI complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,6 +22,7 @@ export default function Header() {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
+  
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
